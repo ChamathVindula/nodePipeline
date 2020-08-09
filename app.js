459f3c440018fa72123cef61ee50d6dcb469654a
Chamath <chamathvin@gmail.com>
2020-08-09 16:31:05 +1000

    second commit

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.port || 3000;
+
 app.get('/', (req, res, next) => {
     res.send("<h1>Welcome to the home page</h1>");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('welcome');
 });
