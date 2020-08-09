7ae4c8ea44d833accb2e23e970b4b1e03d4a4529
Chamath <chamathvin@gmail.com>
2020-08-09 19:34:42 +1000

    seventh commit

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.port || 8080;
-
 app.get('/', (req, res, next) => {
     res.send("<h1>Welcome to the home page</h1>");
 });
 
 app.listen(8080, () => {
-    console.log('welcome to the test web app launch!');
+    console.log('welcome');
 });
