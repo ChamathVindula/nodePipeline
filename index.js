5157c82e5714c83fca49414067d752919f8da794
Chamath <chamathvin@gmail.com>
2020-08-11 17:44:28 +1000

    tenth commit

--- index.js
+++ index.js
@@ -5,47 +5,48 @@ const sequelize = require('./config/database');
 const aws = require('aws-sdk');
 
 // sample import which needs to be handled with a migration
-// const book = require('./models/book');
+const book = require('./models/book');
 
-// const app = express();
+const app = express();
 
-// app.get('/', (req, res, next) => {
-//     res.send("<h1>Welcome to the home page</h1>");
+app.get('/', (req, res, next) => {
+    res.send("<h1>Welcome to the home page</h1>");
+});
+
+sequelize.sync()
+.then(result => {
+    app.listen(8080, () => {
+        console.log(result);
+    });
+})
+.catch(err => {
+    console.log(err);
+})
+
+// (async function(){
+//     try{
+//         aws.config.update({
+//             accessKeyId: 'AKIAZMKREFKAKEGYYFFA',
+//             secretAccessKey: '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',
+//             region: 'us-east-1'
+//         });
+
+//         const s3 = new aws.S3();
+
+//         s3.getObject({
+//             Bucket: 'librarybucket',
+//             Key: 'BengaliBooks/Ghare-Phera - Mahasweta Debi.pdf'
+//         }, function(err, data) {
+//             if (err) {
+//                 console.log("Error", err);
+//             } else {
+//                 console.log("Success", data);
+//         }
 // });
-
-// sequelize.sync()
-// .then(result => {
-//     app.listen(8080, () => {
-//         console.log(result);
-//     });
-// })
-// .catch(err => {
-//     console.log(err);
-// })
-
-(async function(){
-    try{
-        aws.config.update({
-            accessKeyId: 'your access key id',
-            secretAccessKey: 'your secret key',
-            region: 'us-east-1'
-        });
-
-        const s3 = new aws.S3();
-
-        s3.listObjects({
-            Bucket: 'librarybucket'
-        }, function(err, data) {
-            if (err) {
-              console.log("Error", err);
-            } else {
-              console.log("Success", data);
-            }
-          });
-    }
-    catch(e){
-        console.log('error is: ', e)
-    }
-    debugger;
-})();
+//     }
+//     catch(e){
+//         console.log('Error is: ', e)
+//     }
+//     debugger;
+// })();
 
