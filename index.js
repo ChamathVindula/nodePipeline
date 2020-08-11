b07988e8c2f480f086cf92fa451ca730d91edaf2
Chamath <chamathvin@gmail.com>
2020-08-11 18:48:25 +1000

    eleventh commit

--- index.js
+++ index.js
@@ -4,12 +4,52 @@ const sequelize = require('./config/database');
 
 const aws = require('aws-sdk');
 
-// sample import which needs to be handled with a migration
+const fs = require('fs');
+
+//  sample import which needs to be handled with a migration
 const book = require('./models/book');
 
 const app = express();
 
 app.get('/', (req, res, next) => {
+    (async function(){
+        try{
+            aws.config.update({
+                accessKeyId: 'AKIAZMKREFKAKEGYYFFA',
+                secretAccessKey: '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',
+                region: 'us-east-1'
+            });
+    
+            const s3 = new aws.S3();
+    
+            // s3.getObjectsList({
+            //     Bucket: 'libraryBucket'
+            // }, (err, data) => {
+            //     if(err){
+            //         console.log('Error', err);
+            //     } else {
+            //         console.log(data);
+            //     }
+            // })
+            s3.getObject({
+                Bucket: 'librarybucket',
+                Key: 'BengaliBooks/Ghare-Phera - Mahasweta Debi.pdf'
+            }, function(err, data) {
+                if (err) {
+                    console.log("Error", err);
+                } else {
+                    console.log("Success", data);
+                    // console.log(data.Body.toString('ascii'))
+                    // fs.writeFileSync('sampleBook.pdf', data.Body.toString());
+            }
+    });
+        }
+        catch(e){
+            console.log('Error is: ', e)
+        }
+        debugger;
+    })();
+    
     res.send("<h1>Welcome to the home page</h1>");
 });
 
@@ -21,32 +61,4 @@ sequelize.sync()
 })
 .catch(err => {
     console.log(err);
-})
-
-// (async function(){
-//     try{
-//         aws.config.update({
-//             accessKeyId: 'AKIAZMKREFKAKEGYYFFA',
-//             secretAccessKey: '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',
-//             region: 'us-east-1'
-//         });
-
-//         const s3 = new aws.S3();
-
-//         s3.getObject({
-//             Bucket: 'librarybucket',
-//             Key: 'BengaliBooks/Ghare-Phera - Mahasweta Debi.pdf'
-//         }, function(err, data) {
-//             if (err) {
-//                 console.log("Error", err);
-//             } else {
-//                 console.log("Success", data);
-//         }
-// });
-//     }
-//     catch(e){
-//         console.log('Error is: ', e)
-//     }
-//     debugger;
-// })();
-
+});
